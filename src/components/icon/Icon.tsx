82c4b62356d798a4ce1b9a30edc86b5d73cf39d2
Ostap Sulyk <60716383+Ostap-Sulyk@users.noreply.github.com>
2023-03-17 16:00:03 -0400

    Added about me

--- src/components/icon/Icon.tsx
+++ src/components/icon/Icon.tsx
@@ -1,6 +1,9 @@
+import "./Icon.css"
+
+// @ts-ignore
 const Icon = ({ props }) => {
   const { name, website, desc } = props;
-  const light = false;
+  const light = true;
   return (
     <a href={website} target="_blank">
       <img src={light ? `/logos/light/${name}.svg` : `/logos/dark/${name}.svg`} className="logo" alt={desc} />

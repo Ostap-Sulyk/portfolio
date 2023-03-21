529a5c0da22dfa93423a2560b29a261ee82c559a
Ostap Sulyk <60716383+Ostap-Sulyk@users.noreply.github.com>
2023-03-21 19:44:44 -0400

    made some ui changes

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,7 @@ const technologies = [
 
 function App() {
     return (
-        <div className="App theme">
+        <div>
             <div className={"hero"}>
                 <div>
                     <h1>HELLO! 👋🏻</h1>
@@ -43,31 +43,21 @@ function App() {
             <div className='center'>
                 <div>
                     <h2>About Me</h2>
-                    <p>I am a Toronto-based backend developer with a passion for programming and travel. When not
-                        working on software projects, I can often be found practicing mixed martial arts and boxing,
-                        or
-                        playing a game of chess.</p>
-                    <p>I am experienced with a range of technologies including React, MySQL, MongoDB, Docker and
-                        Spring
-                        Boot, and always eager to learn more!</p>
-                    <p>I dream of working as a backend developer and make a positive impact on the industries I work
-                        in.
-                        I am committed to using my skills to create innovative solutions that benefit society and
-                        disrupt traditional ways of doing things.</p>
-                    <p>With a passion for exploring new places and cultures, I am always up for an adventure.
-                        Whether
-                        hiking through the mountains or sampling local cuisine, I constantly seeking new experiences
-                        and
-                        opportunities for personal growth.</p>
-                    <p>Overall, I am a driven and dedicated professional who is committed to making a difference in
-                        the
-                        world.</p>
+                    <p>I am a Toronto-based backend developer with a passion for programming and travel. When not working on software projects, I can often be found practicing mixed martial arts and boxing, or playing a game of chess.</p>
+                    <p>I am experienced with a range of technologies including React, MySQL, MongoDB, Docker and Spring Boot, and always eager to learn more!</p>
+                    <p>I dream of working as a backend developer and make a positive impact on the industries I work in. I am committed to using my skills to create innovative solutions that benefit society and disrupt traditional ways of doing things.</p>
+                    <p>With a passion for exploring new places and cultures, I am always up for an adventure. Whether hiking through the mountains or sampling local cuisine, I constantly seeking new experiences and opportunities for personal growth.</p>
+                    <p>Overall, I am a driven and dedicated professional who is committed to making a difference in the world.</p>
                 </div>
                 <div>
                     <h2>Technologies I Know</h2>
-                    {/* TODO: add animations on hover from react framer motion */}
-                    {technologies.map(e => <Icon key={e.id} props={e} />)}
+                    <div>
+                        {/* TODO: add animations on hover from react framer motion */}
+                        {technologies.map(e => <Icon key={e.id} props={e} />)}
+                    </div>
                 </div>
+
+
             </div>
 
 
--- src/components/icon/Icon.tsx
+++ src/components/icon/Icon.tsx
@@ -1,12 +1,25 @@
+import { useRef } from "react"
+import { useInView } from "framer-motion"
 import "./Icon.css"
 
 // @ts-ignore
 const Icon = ({ props }) => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
   const { name, website, desc } = props;
   const light = true;
   return (
-    <a href={website} target="_blank">
-      <img src={light ? `/logos/light/${name}.svg` : `/logos/dark/${name}.svg`} className="logo" alt={desc} />
+    <a ref={ref} href={website} target="_blank">
+      <span
+        style={{
+          transform: isInView ? "none" : "translateX(-200px)",
+          opacity: isInView ? 1 : 0,
+          transition: "0.3s cubic-bezier(0.17, 0.55, 0.55, .1) .3s"
+        }}
+      >
+        <img src={light ? `/logos/light/${name}.svg` : `/logos/dark/${name}.svg`} className="logo" alt={desc} />
+      </span>
     </a>
   )
 }
